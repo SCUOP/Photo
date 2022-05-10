3fbc6779730075e9be075151641166bf31e10b96
SCUOP <92345079+SCUOP@users.noreply.github.com>
2022-05-10 17:35:17 +0800

    Update random.php

--- random.php
+++ random.php
@@ -2,9 +2,9 @@
 //初始化随机数生成器种子，这行代码也可以删除
 $seed = time();
 //获取随机数
-$num = rand(1,80);
+$num = rand(1,16);
 //拼接图片地址
-$picpath = "https://cdn.jsdelivr.net/gh/dearxuan7392/picture/website/background/".$num.".jpg";
+$picpath = "https://cdn.jsdelivr.net/gh/SCUOP/Photo/blob/main/".$num.".png";
 //重定位到图片
 die(header("Location: $picpath"));
 ?>
